04eb1b8aa955d55a8612c951db1b140faa5cb534
Evgen <evgen@webcat.by>
2019-05-22 13:46:30 +0300

    Added three new screen

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    $('.header_section_btn').click(function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href');
+        var scrollTo = $(id).offset().top;
+        
+        $('html, body').animate({
+            scrollTop: scrollTo
+        }, 1000);
+    });
+
     $('.portfolio_slider').slick({
         slidesToShow: 3,
         appendArrows: $('.portfolio_slider_arrows')
